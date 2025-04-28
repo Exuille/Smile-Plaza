7b14066c7b0f93012e05d09b5ea303d176cf2e5d
herseyy <herseyodasco1@gmail.com>
2025-04-29 03:29:47 +0800

    Add partial ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Home from "./pages/Home"
+
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
