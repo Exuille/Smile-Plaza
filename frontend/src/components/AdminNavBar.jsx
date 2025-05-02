b65a7843ebcf982f00a7d79c51dbada05cd27e05
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-05-02 23:48:19 +0800

    Add NavBar, AdminNavBar

---
+++ frontend/src/components/AdminNavBar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './css/NavBar.css';
+
+const AdminNavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-start">
+        <img src="logo.png" alt="Smile Plaza Logo" className="logo" />
+        <div className="text-container">
+          <h2>Smile Plaza</h2>
+          <h3>Dental Center</h3>
+        </div>
+      </div>
+      <div className="nav-center">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => (isActive ? "nav-link active-link" : "nav-link")}
+        >
+          Home
+        </NavLink>
+        <NavLink 
+          to="/dashboard" 
+          className={({ isActive }) => (isActive ? "nav-link active-link" : "nav-link")}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink 
+          to="/announcement" 
+          className={({ isActive }) => (isActive ? "nav-link active-link" : "nav-link")}
+        >
+          Announcements & Clinic Hours
+        </NavLink>
+        <NavLink 
+          to="/appointment" 
+          className={({ isActive }) => (isActive ? "nav-link active-link" : "nav-link")}
+        >
+          Appointments
+        </NavLink>
+      </div>
+      <div className="nav-end">
+        <NavLink 
+          to="/" 
+          className="login-btn"
+        >
+          Login
+        </NavLink>
+        <NavLink 
+          to="/" 
+          className="signup-btn"
+        >
+          Sign Up
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default AdminNavBar;
